655983c3dff590489ca0271f4718ab5133923f32
kimhyeri <hyer1k@naver.com>
2018-08-08 23:45:53 +0900

    codeNum scroll delegate

--- Number/New Group/CodeNumViewController.swift
+++ Number/New Group/CodeNumViewController.swift
@@ -13,10 +13,10 @@ class CodeNumViewController: UIViewController, UIGestureRecognizerDelegate {
 
     var interactor = Interactor()
 
+    @IBOutlet weak var firstView: UIView!
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var changeView: UIView!
     @IBOutlet weak var thirdView: UIView!
-    
+    @IBOutlet weak var nameLabel: UILabel!
     
     @IBAction func pressedSosik(_ sender: Any) {
         let nv = self.storyboard?.instantiateViewController(withIdentifier: "LeftMenuNavigationController")
@@ -24,23 +24,14 @@ class CodeNumViewController: UIViewController, UIGestureRecognizerDelegate {
     }
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        thirdView.alpha = 0
         let panGesture = UIPanGestureRecognizer(target: self, action: #selector(self.handlePanGesture(_:)))
-        
-        self.changeView.addGestureRecognizer(panGesture)
-        
+        self.view.addGestureRecognizer(panGesture)
         let menuLeftNavigationController = storyboard!.instantiateViewController(withIdentifier: "LeftMenuNavigationController") as! UISideMenuNavigationController
         SideMenuManager.default.menuLeftNavigationController = menuLeftNavigationController
-
-        let label = UILabel()
-        label.text = ""
         imageView.backgroundColor = .black
-        
         addButton()
-        
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
@@ -65,51 +56,23 @@ class CodeNumViewController: UIViewController, UIGestureRecognizerDelegate {
         let storyboard = UIStoryboard(name: "MyPage", bundle: nil)
         let vc = storyboard.instantiateViewController(withIdentifier: "MyPage") as! MyPageViewController
         self.navigationController?.pushViewController(vc, animated: true)
-        
     }
     
     @objc func handlePanGesture(_ sender: UIScreenEdgePanGestureRecognizer){
         let location = sender.translation(in: view)
         let progress = -(location.y / self.view.frame.height)
-
+        print(location)
         switch sender.state {
         case .changed:
             print("changed")
-            thirdView.alpha = progress * 3
+            nameLabel.text = "희은님"
+            imageView.frame = CGRect(x: 20, y: 50, width: 30, height: 30)
+//            nameLabel.frame = CGRect(x: 100, y: 10, width: view.frame.width/2, height: 30)
+            firstView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 120)
         default:
             print("default")
         }
     }
-
-    
-//    @objc func handlePanGesture(_ sender: UIScreenEdgePanGestureRecognizer){
-//        let location = sender.translation(in: view)
-//        let progress = -(location.y / self.view.frame.height)
-//        print(progress)
-//
-//        switch sender.state {
-//        case .began:
-//            print("began")
-//            interactor.hasStarted = true
-//            let nextVC = UIStoryboard(name: "Home", bundle: nil).instantiateViewController(withIdentifier: "Home") as! HomeViewController
-//            nextVC.interactor = self.interactor
-//            nextVC.transitioningDelegate = nextVC
-//            self.present(nextVC, animated: true, completion: nil)
-//        case .changed:
-//            print("changed")
-//            interactor.shoudFinish = progress > 0.5
-//            interactor.update(progress)
-//        case .cancelled:
-//            print("cancelled")
-//            interactor.hasStarted = false
-//        case .ended:
-//            interactor.hasStarted = false
-//            interactor.shoudFinish ? interactor.finish() : interactor.cancel()
-//        default:
-//            print("default")
-//        }
-//    }
-    
 }
 
 extension HomeViewController: UIViewControllerTransitioningDelegate {
