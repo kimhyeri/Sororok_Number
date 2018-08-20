b1988f855abdc2f2c4004bfab8f3cb892f6fee6f
kimhyeri <hyer1k@naver.com>
2018-08-20 16:06:42 +0900

    #3 Connect naver login

--- Number/ViewController.swift
+++ Number/ViewController.swift
@@ -7,7 +7,9 @@
 //
 
 import UIKit
+import Alamofire
 import GoogleSignIn
+import NaverThirdPartyLogin
 
 class ViewController: UIViewController {
 
@@ -15,20 +17,66 @@ class ViewController: UIViewController {
     @IBOutlet weak var naver: UIButton!
     @IBOutlet weak var kakao: UIButton!
     
+    let loginInstance = NaverThirdPartyLoginConnection.getSharedInstance()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        changeView()
+    }
 
+    func changeView(){
         google.layer.cornerRadius = 10
         naver.layer.cornerRadius = 10
         kakao.layer.cornerRadius = 10
-        
-//
-//        GIDSignIn.sharedInstance().uiDelegate = self
-//
-//        let googleSignInbutton = GIDSignInButton()
-//        googleSignInbutton.center = view.center
-//        view.addSubview(googleSignInbutton)
+    }
+    
+    @IBAction func naverButtonPressed(_ sender: UIButton){
+        loginInstance?.delegate = self
+        loginInstance?.requestThirdPartyLogin()
+    }
+}
+
+extension ViewController: NaverThirdPartyLoginConnectionDelegate{
+    func oauth20ConnectionDidOpenInAppBrowser(forOAuth request: URLRequest!) {
+        let naverSignInViewController = NLoginThirdPartyOAuth20InAppBrowserViewController(request: request)!
+        present(naverSignInViewController, animated: true, completion: nil)
     }
 
+    func oauth20ConnectionDidFinishRequestACTokenWithAuthCode() {
+        print("Success oauth20ConnectionDidFinishRequestACTokenWithAuthCode")
+        getNaverEmailFromURL()
+
+    }
+
+    func oauth20ConnectionDidFinishRequestACTokenWithRefreshToken() {
+        print("Success oauth20ConnectionDidFinishRequestACTokenWithRefreshToken")
+        getNaverEmailFromURL()
+
+    }
+
+    func oauth20ConnectionDidFinishDeleteToken() {
+        
+    }
+
+    func oauth20Connection(_ oauthConnection: NaverThirdPartyLoginConnection!, didFailWithError error: Error!) {
+        print(error.localizedDescription)
+        print(error)
+    }
+
+    func getNaverEmailFromURL(){
+        guard let loginConn = NaverThirdPartyLoginConnection.getSharedInstance() else {return}
+        guard let tokenType = loginConn.tokenType else {return}
+        guard let accessToken = loginConn.accessToken else {return}
+        
+        let authorization = "\(tokenType) \(accessToken)"
+        Alamofire.request("https://openapi.naver.com/v1/nid/me", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: ["Authorization" : authorization]).responseJSON { (response) in
+            guard let result = response.result.value as? [String: Any] else {return}
+            guard let object = result["response"] as? [String: Any] else {return}
+            guard let number = object["number"] as? Int else {return}
+            guard let name = object["name"] as? String else {return}
+            guard let email = object["email"] as? String else {return}
+            print(result)
+        }
+    }
 }
 
--- Pods/naveridlogin-sdk-ios/NaverThirdPartyLogin.framework/Headers/NaverThirdPartyConstantsForApp.h
+++ Pods/naveridlogin-sdk-ios/NaverThirdPartyLogin.framework/Headers/NaverThirdPartyConstantsForApp.h
@@ -32,8 +32,8 @@ typedef enum {
     OK
 } OAuthLoginState;
 
-#define kServiceAppUrlScheme    @"thirdparty20samplegame"
+#define kServiceAppUrlScheme    @"sororok"
 
-#define kConsumerKey            @"jyvqXeaVOVmV"
-#define kConsumerSecret         @"527300A0_COq1_XV33cf"
-#define kServiceAppName         @"네이버 아이디로 로그인"
+#define kConsumerKey            @"MzS8vVP3PKB6EY2PBDNs"
+#define kConsumerSecret         @"LMWhVh7OVW"
+#define kServiceAppName         @"Number"
