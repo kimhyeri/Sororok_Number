9ecec60e85e0208f85759eb41c84109551e10d8e
kimhyeri <hyer1k@naver.com>
2018-08-24 00:28:19 +0900

    Set default back button

---
+++ Number/Home/Helper.swift
@@ -0,0 +1,19 @@
+//
+//  Helper.swift
+//  Number
+//
+//  Created by hyerikim on 2018. 8. 24..
+//  Copyright © 2018년 nexters.number. All rights reserved.
+//
+
+import Foundation
+
+extension UIViewController {
+    func initNav(){
+        self.navigationController?.navigationBar.backIndicatorImage = UIImage(named: "btnCommBackWh")
+        self.navigationController?.navigationBar.backIndicatorTransitionMaskImage = UIImage(named: "btnCommBackWh")
+        self.navigationItem.backBarButtonItem = UIBarButtonItem(title:"", style:.plain, target:nil, action:nil)
+        self.navigationController?.navigationBar.tintColor = UIColor.white
+
+    }
+}
--- Number/Home/Setting/SettingViewController.swift
+++ Number/Home/Setting/SettingViewController.swift
@@ -17,18 +17,9 @@ class SettingViewController: UIViewController , UITableViewDataSource, UITableVi
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
-        self.navigationController?.navigationBar.tintColor = UIColor.white
-        var image = UIImage(named: "btnCommBackWh")
-        image = image?.withRenderingMode(.alwaysOriginal)
-        self.navigationController?.navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .plain, target: nil, action: nil)
-    
+        initNav()
         let height = 20 + (self.navigationController?.navigationBar.frame.height)!
         naviView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: height)
-    }
-    
-    @objc func goBack() {
-        print("go")
-        self.navigationController?.dismiss(animated: true, completion: nil)
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -48,3 +39,4 @@ class SettingViewController: UIViewController , UITableViewDataSource, UITableVi
         self.present(popUp, animated: false, completion: nil)
     }
 }
+
