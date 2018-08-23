46b9f29a3dfbf6119d1465ba218af3cce199f85a
kimhyeri <hyer1k@naver.com>
2018-08-24 01:30:18 +0900

    Add right nav button

--- Number/Home/Group/GroupCreateViewController.swift
+++ Number/Home/Group/GroupCreateViewController.swift
@@ -23,10 +23,16 @@ class GroupCreateViewController: UIViewController {
         initNav()
         self.title = "그룹생성"
         self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
+        let saveButton = UIBarButtonItem(title: "저장",  style: .plain, target: self, action: #selector(self.saveButton))
+        self.navigationItem.rightBarButtonItem = saveButton
         groupCode.layer.cornerRadius = 10
         groupImage.backgroundColor = .black
         groupImage.layer.cornerRadius = self.groupImage.frame.width/2
         groupView.layer.cornerRadius = 10
+    }
+    
+    @objc func saveButton(){
+        print("Saved")
     }
 
     @IBAction func clipButtonPressed(_ sender: Any) {
--- Number/Home/MyPage/MyPageViewController.swift
+++ Number/Home/MyPage/MyPageViewController.swift
@@ -9,16 +9,30 @@
 import UIKit
 
 class MyPageViewController: UIViewController {
-
+    
     @IBOutlet weak var myImage: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initNav()
+        defaultView()
+    }
+    
+    func defaultView(){
+        self.title = "마이페이지"
+        self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
         myImage.layer.cornerRadius = myImage.frame.width/2
         myImage.backgroundColor = .black
+        let saveButton = UIBarButtonItem(title: "저장",  style: .plain, target: self, action: #selector(self.saveButton))
+        self.navigationItem.rightBarButtonItem = saveButton
         self.navigationController?.navigationBar.tintColor = UIColor.white
     }
-
+    
+    @objc func saveButton(){
+        print("Saved server")
+    }
+    
+    
     @IBAction func buttonPressed(_ sender: Any) {
         let popUp = UIStoryboard(name: "Logout", bundle: nil).instantiateViewController(withIdentifier: "Logout") as! LogoutViewController
         popUp.modalPresentationStyle = .overCurrentContext
