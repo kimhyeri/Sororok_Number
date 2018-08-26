9b190f7afc2ef926a5bf0e56d6a334b68a51a9f3
kimhyeri <hyer1k@naver.com>
2018-08-26 20:40:52 +0900

    view up when text editing

--- Number/Home/MyPage/MyPageViewController.swift
+++ Number/Home/MyPage/MyPageViewController.swift
@@ -8,9 +8,15 @@
 
 import UIKit
 
-class MyPageViewController: UIViewController {
+class MyPageViewController: UIViewController, UITextFieldDelegate {
     
+    @IBOutlet weak var textView: UIView!
     @IBOutlet weak var myImage: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var numText: UITextField!
+    
+    var defaultFrame : CGRect?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +25,16 @@ class MyPageViewController: UIViewController {
     }
     
     func defaultView(){
+        nameText.delegate = self
+        numText.delegate = self
+        emailText.delegate = self
+        
         self.title = "마이페이지"
+        defaultFrame = textView.frame
+        nameText.attributedPlaceholder = NSAttributedString(string: "이름", attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
+        emailText.attributedPlaceholder = NSAttributedString(string: "이메일", attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
+        numText.attributedPlaceholder = NSAttributedString(string: "전화번호", attributes: [NSAttributedStringKey.foregroundColor: UIColor.white])
+        
         self.navigationController?.navigationBar.titleTextAttributes = [.foregroundColor: UIColor.white]
         myImage.layer.cornerRadius = myImage.frame.width/2
         myImage.backgroundColor = .black
@@ -28,15 +43,27 @@ class MyPageViewController: UIViewController {
         self.navigationController?.navigationBar.tintColor = UIColor.white
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?){
+        self.view.endEditing(true)
+        textView.frame = defaultFrame!
+    }
+    
     @objc func saveButton(){
         print("Saved server")
         self.navigationController?.popViewController(animated: true)
     }
     
-    
     @IBAction func buttonPressed(_ sender: Any) {
         let popUp = UIStoryboard(name: "Logout", bundle: nil).instantiateViewController(withIdentifier: "Logout") as! LogoutViewController
         popUp.modalPresentationStyle = .overCurrentContext
         self.present(popUp, animated: false, completion: nil)
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        viewUp()
+    }
+    
+    func viewUp() {
+        textView.frame = CGRect(x: 0, y: 65, width: self.view.frame.width, height: textView.frame.height)
     }
 }
