8fbf835c58b116ee1d96cd89e9076f94385b0fff
kimhyeri <hyer1k@naver.com>
2018-10-08 15:24:55 +0900

    Create sidemenu view

--- Number/Home/SideMenu/Controller/SideMenuTableViewController.swift
+++ Number/Home/SideMenu/Controller/SideMenuTableViewController.swift
@@ -19,37 +19,6 @@ class SideMenuTableViewController: UITableViewController {
         loadData(memberId: UserDefaults.standard.integer(forKey: "memberId"))
         tableView.rowHeight = UITableViewAutomaticDimension
     }
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if let count = historyData?.historyList.count {
-            return count
-        } else {
-            return 1
-        }
-    }
-
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! SideMenuCell
-        if historyData?.historyList.count == 0 {
-            cell.iconImage.image = UIImage(named: "icnNoticeEmpty")
-            cell.news.text = "최근 소식이 없습니다."
-            cell.newsTimeLabel.text = " "
-        }else {
-            cell.iconImage.image = UIImage(named: "icnNotiNotice")
-            cell.news.text = self.historyData?.historyList[indexPath.row].content
-            cell.newsTimeLabel.text = self.historyData?.historyList[indexPath.row].date
-        }
-        
-        return cell
-    }
-    
-    override func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UITableViewAutomaticDimension
-    }
     
     @IBAction func exitButtonPressed(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
